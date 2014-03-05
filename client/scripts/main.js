af41d0f82be1401962ba97492881237579f3f57f
Tony Pujals <tony@subfuzion.com>
2014-03-05 10:06:25 -0800

    refactored main app start

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,28 +1,33 @@
-// models
-var App = require('./models/App')
-  ;
-
-// views
-var ServerControlView = require('./views/ServerControlView')
-  , MessagePanelView = require('./views/MessagePanelView')
-  ;
-
-
-var app = new App();
-
-// wire up views
-
-var serverControlView = new ServerControlView({
-  model: app,
-  el: '#server-control'
-});
-
-var messagePanelView = new MessagePanelView({
-  model: app,
-  el: '#message-panel'
-});
-
 module.exports = {
-  app: app
+  start: function () {
+    // models
+    var App = require('./models/App')
+      ;
+
+    // views
+    var ServerControlView = require('./views/ServerControlView')
+      , MessagePanelView = require('./views/MessagePanelView')
+      ;
+
+
+    var app = new App();
+
+    // wire up views
+    // just creating them works since they wire
+    // up the page and render when initialized
+
+    new ServerControlView({
+      model: app,
+      el: '#server-control'
+    });
+
+    new MessagePanelView({
+      model: app,
+      el: '#message-panel'
+    });
+
+    // all wired up to the page and ready for user input
+  }
 };
+
 
