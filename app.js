9c19541dc24a465523e5f2dd288567a1e4634a4b
Tony Pujals <tonypujals@google.com>
2022-07-19 02:56:28 -0700

    updated to latest versions

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express')
   , routes = require('./routes')
+  , bodyParser = require('body-parser')
+  , errorhandler = require('errorhandler')
+  , favicon = require('serve-favicon')
+  , methodOverride = require('method-override')
+  , morgan = require('morgan')
   , http = require('http')
   , path = require('path')
   , echo = require('echo.io')
@@ -8,21 +13,19 @@ var express = require('express')
 
 var app = express();
 
-app.configure(function () {
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(app.router);
-});
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(methodOverride());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+// TODO: add favicon.ico
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-app.configure('development', function () {
-  app.use(express.errorHandler());
-});
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorhandler());
+}
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
